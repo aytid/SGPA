ea0b17a2a72d1d23ab5166301515578466da1f8c
aytid <124620798+aytid@users.noreply.github.com>
2023-04-01 18:39:49 +0530

    Update sgpa.py

--- sgpa.py
+++ sgpa.py
@@ -1,8 +1,8 @@
 import streamlit as st
 st.title("SPGA Estimator")
-c=st.number_input("Enter your present CGPA",min_value=0,step=1.0,format="%f")
-n=st.number_input("Enter number of Semesters held",min_value=1)
-e=st.number_input("Enter your target CGPA after this semester",min_value=0,step=1.0,format="%f")
+c=st.number_input("Enter your present CGPA",format="%f")
+n=st.number_input("Enter number of Semesters held")
+e=st.number_input("Enter your target CGPA after this semester",format="%f")
 s=0
 x=round(((n*c)+s)/(n+1))
 while x<=e:
