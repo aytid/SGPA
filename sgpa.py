07f71f8b3cbd8e57f92f371875103bbd8fefc6a4
aytid <124620798+aytid@users.noreply.github.com>
2023-04-01 18:34:59 +0530

    Update sgpa.py

--- sgpa.py
+++ sgpa.py
@@ -1,15 +1,14 @@
 import streamlit as st
-
 st.title("SPGA Estimator")
-
-c = st.number_input("Enter your present CGPA", min_value=0, step=1, format="%f")
-n = st.number_input("Enter number of Semesters held", min_value=1)
-e = st.number_input("Enter your expecting CGPA", min_value=0, step=1, format="%f")
-
-s = 0
-x = round(((n * c) + s) / (n + 1),2)
-
-while x <= e:
-    s += 0.1
-
-st.write("You need to score ", s, " in your next semester")
+c=st.number_input("Enter your present CGPA",min_value=0,step=1.0,format="%f")
+n=st.number_input("Enter number of Semesters held",min_value=1)
+e=st.number_input("Enter your target CGPA after this semester",min_value=0,step=1.0,format="%f")
+s=0
+x=round(((n*c)+s)/(n+1))
+while x<=e:
+	s+=0.01
+	x = round(((n * c) + s) / (n + 1),2)
+if s>10:
+	st.write("You cannot reach your espected CGPA!!")
+else:
+	st.write("You need to score minimum of ",s," SGPA in your next semester")
